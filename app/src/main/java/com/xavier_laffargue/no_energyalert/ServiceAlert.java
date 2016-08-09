ccaad01d8234c7fba6fb411a256a609f0cfef269
Xavier Laffargue <laffargue.xavier@gmail.com>
2016-08-09 16:37:52 +0200

    Feature : send SMS

--- app/src/main/java/com/xavier_laffargue/no_energyalert/ServiceAlert.java
+++ app/src/main/java/com/xavier_laffargue/no_energyalert/ServiceAlert.java
@@ -4,6 +4,8 @@ package com.xavier_laffargue.no_energyalert;
  * Created by Xavier on 05/10/15.
  */
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,6 +17,7 @@ import android.os.IBinder;
 import android.telephony.SmsManager;
 import android.text.format.Time;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -37,11 +40,14 @@ import java.util.Locale;
 
 public class ServiceAlert extends Service {
 
-    public JSONParser jsonParser = new JSONParser();
+
     private int historyPlugged = 0;
     private FileSetting fileSetting;
+    public JSONParser jsonParser = new JSONParser();
+    private String urlAPI = "http://37.187.47.89/noenergy/index.php";
     public static final String BROADCAST_ACTION = "CHANGE_VALUE_UI";
     public static final String TAG_LOG = "ALERT_SERVICE";
+
 
     /**
      * Lancement du service
@@ -87,13 +93,14 @@ public class ServiceAlert extends Service {
             if(historyPlugged != plugged) {
                 if (plugged == 1) {
                     Log.d(TAG_LOG, "Connected");
+                    sendSMS(fileSetting.readNumberPhone(getApplicationContext()), "Reprise");
                     intentUI.putExtra("TEXT", "Branché " + dateFormatDDMMYYYY.format(new Date()));
                     new CallAPI().execute("work");
 
                 } else {
                     Log.d(TAG_LOG, "Alerte");
+                    sendSMS(fileSetting.readNumberPhone(getApplicationContext()), "Panne");
                     intentUI.putExtra("TEXT", "Panne " + dateFormatDDMMYYYY.format(new Date()));
-                    sendSms("Panne électrique depuis " + dateFormatDDMMYYYY.format(new Date()));
                     new CallAPI().execute("breakdown");
 
 
@@ -116,16 +123,73 @@ public class ServiceAlert extends Service {
     }
 
     /**
-     * Envoit un sms au numero inscrit dans le fichier constante FILENAME de SettingActivity
-     * @param message Message à envoyer
+     *
+     * @param phoneNumber
+     * @param message
      */
-    private void sendSms(String message) {
-        Log.d(TAG_LOG, "Number : " + fileSetting.readNumberPhone(getApplicationContext()));
-        SmsManager manager = SmsManager.getDefault();
-        manager.sendTextMessage(fileSetting.readNumberPhone(getApplicationContext()), null, message, null, null);
+    private void sendSMS(String  phoneNumber, String  message)
+    {
+        String  SENT = "SMS_SENT";
+        String  DELIVERED = "SMS_DELIVERED";
 
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
 
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context  arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context  arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
     }
+
 
     class CallAPI extends AsyncTask<String, String, String> {
 
@@ -154,8 +218,8 @@ public class ServiceAlert extends Service {
             // Note that create product url accepts POST method
             JSONObject json = jsonParser.makeHttpRequest("http://37.187.47.89/noenergy/index.php", "POST", params);
 
-            // check log cat fro response
-            Log.d("Create Response", json.toString());
+            // check log cat for response
+            Log.d("Response", json.toString());
 
             // check for success tag
             try {
